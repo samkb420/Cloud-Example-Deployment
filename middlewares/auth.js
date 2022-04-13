3413f856a7a2024b935f06178f8a8069f73d7605
Samkb420 <kagosamuel07@yahoo.com>
2022-04-14 02:43:06 +0300

    Added branch for code Base~

---
+++ middlewares/auth.js
@@ -0,0 +1,20 @@
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/home");
+  }
+  next();
+}
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    
+    // return next();
+    return res.redirect("/home");
+  }
+  res.redirect("/login");
+}
+
+module.exports = {
+  checkNotAuthenticated,
+  checkAuthenticated,
+};
